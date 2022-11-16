195f3eec5ea50f62e097da127d17d51ace8adfad
Griffin Moyer <gmoyer@goldensoftware.com>
2022-11-15 21:30:17 -0700

    Updated logo

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 
-
+var scrollDiv = 8; //how fast the scroll
+var scrollMul = 2; //how many frames to skip
+var scrollRatio = scrollMul/scrollDiv;
 
 $(window).scroll(function (event) {
     // Change animation position based on scrolling
-    var scroll = ($(window).scrollTop()/2)+1;
-    var scrollstr = "";
-    if (scroll < 1000)
-        scrollstr += "0";
+    var scroll = parseInt($(window).scrollTop()/scrollDiv)*scrollMul;
+    scroll++; //because the first value is 1
+    var scrollstr = "0";
     if (scroll < 100)
         scrollstr += "0";
     if (scroll < 10)
         scrollstr += "0";
     if (scroll > 250)
         scroll = 250;
-    scrollstr += parseInt(scroll);
-    $("#logoImg").attr("src", "animation/" + scrollstr + ".jpg");
+    scrollstr += scroll;
+    $("#logoImg").attr("src", "animation/" + scrollstr + ".png");
 
 
     //animate menu when reached last frame
@@ -33,10 +34,22 @@ $(window).scroll(function (event) {
             newTop += 200
         $(this).css('top', newTop + '%');
     });
+
+
+    //shift up menu if reached bottom
+    var excess = $(window).scrollTop() - (1/scrollRatio*250);
+    if (excess > 0) {
+        $('#menu').css('top', 'calc(50% - ' + excess + 'px)');
+        $('#logoImg').css('top', 'calc(50% - ' + excess + 'px)');
+    }
 });
 
 //background squares
 $(document).ready(function () {
+    //set up the document height
+    var docHeight = window.innerHeight + (1/scrollRatio*250);
+    $('body').css("height", docHeight + "px");
+    $('#menu-box').css("top", docHeight + "px");
     for (let i = 0; i < 30; i++)
         $('#background').append('<div class="background-square" style="z-index: ' + (-i) + '"></div>');
 
@@ -60,9 +73,16 @@ $(document).ready(function () {
 
     $( ".menu-item" ).hover(
         function() {
-          $( this ).animate({"font-size": "2.7em"}, 500);
+          $( this ).animate({"font-size": "2.7em"}, 200);
         }, function() {
-          $( this ).animate({"font-size": "2.2em"}, 500);
+          $( this ).animate({"font-size": "2.2em"}, 200);
         }
       );
 });
+
+//readjust the doc height when the window is resized
+$( window ).resize(function() {
+    var docHeight = window.innerHeight + (1/scrollRatio*250);
+    $('body').css("height", docHeight + "px");
+    $('#menu-box').css("top", docHeight + "px");
+  });
